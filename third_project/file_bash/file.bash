ebf0e0fa9b286e62e4aa5792a5adb8443c8e5b44
federovighi <136468971+federovighi@users.noreply.github.com>
2024-02-10 15:42:41 +0100

    Add files via upload

---
+++ third_project/file_bash/file.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu
+#SBATCH --time 24:00:00
+#SBATCH --mem=32gb
+#SBATCH --ntasks-per-node 10
+
+
+#< Charge resources to account 
+#SBATCH --account T_2022_DLFEA
+
+echo $SLURM_JOB_NODELIST
+
+echo  #OMP_NUM_THREADS : $OMP_NUM_THREADS
+
+module load miniconda3
+source "$CONDA_PREFIX/etc/profile.d/conda.sh"
+conda activate myenv
+
+#choose the correct path on the machine
+python ./yolov3-KAIST-master/train_kaist_multi.py
+
+conda deactivate
